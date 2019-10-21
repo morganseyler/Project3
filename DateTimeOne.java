f46fa4d29fa4527f0e540deae819a1e6e849abdd
Morgan Seyler <morganseyler@ou.edu>
2019-10-21 13:35:47 -0500

    Completed getValueOfSecond by using SimpleDateFormat and only printing the time in seconds

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -6,16 +6,19 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
    int getValueOfSecond()
    {
 	   //change value to intended getValueOfSecond
-	   long value = System.currentTimeMillis();
-
-	   return (int) value;
+	   Date date1 = new Date();
+	   SimpleDateFormat formatter = new SimpleDateFormat("ss");
+	   String strDate = formatter.format(date1);
+	   int value = Integer.parseInt(strDate);
+	   System.out.println("The value of Second now: " + value);
+	   return value;
    }
    
    void dateTimeNow()
    {
 	   Date date1 = new Date();
 	   SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
-	   String strDate= formatter.format(date1);
+	   String strDate = formatter.format(date1);
 	   System.out.println("Current Date/Time: " + strDate);
 	   
 

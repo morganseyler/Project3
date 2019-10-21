4cf42d5486fdff9c4a8229f668346a182935ef2e
Morgan Seyler <morganseyler@ou.edu>
2019-10-21 13:28:54 -0500

    Finished dateTimeNow method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,15 +1,24 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
    int getValueOfSecond()
    {
 	   //change value to intended getValueOfSecond
+	   long value = System.currentTimeMillis();
 
+	   return (int) value;
    }
    
    void dateTimeNow()
    {
+	   Date date1 = new Date();
+	   SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+	   String strDate= formatter.format(date1);
+	   System.out.println("Current Date/Time: " + strDate);
 	   
+
    }
    
    void sleepForFiveSec()
--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -1,5 +1,15 @@
 
 public class MesoAsciiCal extends MesoAsciiAbstract 
 {
+
+	public MesoAsciiCal(MesoStation mesoStation) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	int calAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
    
 }

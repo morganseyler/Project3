4e5ed178ecebec8718b03d51177e1a54ed8066e1
Morgan Seyler <morganseyler@ou.edu>
2019-10-21 11:17:53 -0500

    Added all files to gitHub and created abstract methods in DateTimeOne

---
+++ DateTimeOne.java
@@ -0,0 +1,34 @@
+
+public class DateTimeOne extends MesoDateTimeOneAbstract
+{
+   int getValueOfSecond()
+   {
+	   //change value to intended getValueOfSecond
+
+   }
+   
+   void dateTimeNow()
+   {
+	   
+   }
+   
+   void sleepForFiveSec()
+   {
+	   
+   }
+   
+   void dateTimeOfOtherCity()
+   {
+	   
+   }
+   
+   void dateTimeDifferentZone()
+   {
+	   
+   }
+   
+   void timeZoneHashMap()
+   {
+	   
+   }
+}

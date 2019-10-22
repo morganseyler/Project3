88be23565de66458ce2aa91d72cca3dff6800d82
Morgan Seyler <morganseyler@ou.edu>
2019-10-22 11:30:26 -0500

    Created Array arr to store  the date and time of each timeZone

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,13 +1,18 @@
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract {
 	
 	//First HashMap with initial TimeZones
 	HashMap<String, String> timeZoneMap1 = new HashMap<String, String>();
+	//Second HashMap with first sorted TimeZones	
+	HashMap<String, String> timeZoneMap2 = new HashMap<String, String>();
 	
 	public int getValueOfSecond() {
 		Date date1 = new Date();
@@ -57,7 +62,7 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 
 	public void dateTimeDifferentZone() {
 
-		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm ");
+		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
 		// Time and date at GMT
 		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
@@ -73,7 +78,10 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 		formatter.setTimeZone(TimeZone.getTimeZone("GMT-5"));
 		timeZoneMap1.put("CST", formatter.format(new Date()));
 		System.out.println("CST: " + formatter.format(new Date()));
-		
+	}
+
+	public void timeZoneHashMap() {
+		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 		//Add 2 additional timeZones to timeZoneMap
 		
 		// Time and date at ZST
@@ -83,18 +91,38 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 		// Time and date at AST
 		formatter.setTimeZone(TimeZone.getTimeZone("GMT+8"));
 		timeZoneMap1.put("AST", formatter.format(new Date()));
-	}
+		
+		//Array for LocalDateTime
+		//Figure out  how to convert from String to LocalDateTime
+		LocalDateTime[] arr = new LocalDateTime[5];
+		
+		//Test zone
+		int i = 0;
+		for (Entry<String, String> entry : timeZoneMap1.entrySet()) {
+		    String key = entry.getKey();
+		    //String value = entry.getValue();
+		    
+			String str = timeZoneMap1.get(key);
+			DateTimeFormatter formatStyle = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
+			LocalDateTime dateTime = LocalDateTime.parse(str, formatStyle);
+			arr[i] = dateTime;
+			System.out.println(arr[i]);
+			i++;
+		}
 
-	public void timeZoneHashMap() {
-		System.out.println(timeZoneMap1);
+
+
+		
+		//Test Zone
+		
 		//Sorted Style 1
 		System.out.println("Print Style 1:");
 		
 		//Sorted Style 2
-		System.out.println("Print Style 2:");
+		System.out.println("Print Style 3:");
 		
 		//Sorted Style 3
-		System.out.println("Print Style 3:");
+		System.out.println("Print Style 5:");
 		
 	}
 }

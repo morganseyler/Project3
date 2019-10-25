6f978417e89b4dff670590ede608fc5f616ff06b
Morgan Seyler <morganseyler@ou.edu>
2019-10-24 20:03:22 -0500

    Currently working on compareYear, converting the String dates into DateTime

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -6,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Locale;
 
 
@@ -16,33 +21,57 @@ public class DateTimeTwo {
 	private String fifteenthDay;
 	private String lastDay;
 	
+	private HashMap<LocalDate, Integer> map = new HashMap<LocalDate, Integer>();
+	
 	public void daysOfCurrentMonth() {
 	    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
-		
-	    Date d1 = new Date();
-//	    Calendar calendar = new GregorianCalendar();
-//	    calendar.get(Calendar.DAY_OF_MONTH);
 	    
-	    
-		d1.setDate(10);
-	    tenthDay = simpleDateFormat.format(d1).toUpperCase();
-	    
-		Date d2 = new Date();
-		d2.setDate(18);
-		eighteenthDay = simpleDateFormat.format(d2).toUpperCase();
+	    Calendar date = Calendar.getInstance();
+	    date.set(Calendar.DAY_OF_MONTH, 10);
+	    Calendar date1 = Calendar.getInstance();
+	    date1.set(Calendar.DAY_OF_MONTH, 18);
+
+	    tenthDay = simpleDateFormat.format(date.getTime()).toUpperCase();
+
+		eighteenthDay = simpleDateFormat.format(date1.getTime()).toUpperCase();
 
 		System.out.println("The tenth day of this month is " + tenthDay + " and eighteenth is " + eighteenthDay);
 	}
 
 	public void daysOfAnyMonth(int month, int year) {
-		DateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
+	    Calendar date = Calendar.getInstance();
 
+	    date.set(Calendar.MONTH, month);
+	    date.set(Calendar.YEAR, year);
+
+	    date.set(Calendar.DAY_OF_MONTH, 15);
+	    System.out.println(date.get(Calendar.YEAR));
+	    System.out.println(date.get(Calendar.MONTH));
+	    System.out.println(date.get(Calendar.DAY_OF_MONTH));
+	    fifteenthDay = simpleDateFormat.format(date.getTime()).toUpperCase();
+	    
+	    date.getActualMaximum(Calendar.DAY_OF_MONTH);
+	    System.out.println("NEW: " + date.getActualMaximum(Calendar.DAY_OF_MONTH));
+		
 		System.out.println("For the year (" + year + ") and month (" + month + "), the fifteenth day is " + fifteenthDay + " and the last day is " + lastDay);
 
 	}
 
-	public void compareYear() {
+	public void compareYear() throws IOException {
+		String filename = "Dates.txt";
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+    	String lineOfData = br.readLine();	
+
+		while (lineOfData!=null)
+    	{
+
+			
+			// Get the next line of the file
+			lineOfData = br.readLine();
+    	}
 		
+		br.close();
 		
 	}
 

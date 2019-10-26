94ca30fef4a3a39f1eb8bbe0abab34d5a42f40ce
Morgan Seyler <morganseyler@ou.edu>
2019-10-26 11:52:51 -0500

    Finished MesoLexicographical sort using asciiVal, working on unsorted

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,12 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class MesoLexicographical extends MesoSortedAbstract
 {
+
+	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
+
+        TreeMap<String, Integer> sortList = new TreeMap<>(asciiVal);
+        Set<Entry<String, Integer>> getList = sortList.entrySet();
+        
+        for(Entry<String, Integer> sort : getList){
+            System.out.println(sort.getKey());
+        }
+		
+	}
+
 	@Override
 	Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 }
